d7a41bcd6cea86f5e21e90dcc27a58393fe1d1ef
Romitou <romitou@protonmail.com>
2020-10-17 23:08:37 +0200

    :tada: Initial commit.

---
+++ layouts/default.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+
+      <v-img
+        src="https://cache.filevirtuelle.parcasterix.fr/images/pages/nav.jpg"
+        height="175"
+      ></v-img>
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-star</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Attractions</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-account-box</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Compte</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Se connecter</v-list-item-title>
+        </v-list-item>
+        <v-divider class="ma-2"></v-divider>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-help</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Comment utiliser ?</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Filotomatix'
+    }
+  }
+}
+</script>
