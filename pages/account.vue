7261e3233b70bf5b383a4b8ce56645f0da4fdc36
Romitou <romitou@protonmail.com>
2020-10-20 23:33:27 +0200

    :sparkles: Introducing QR Code display

---
+++ pages/account.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="mt-3">
+    <v-card
+      class="d-flex justify-center mb-6"
+      flat
+    >
+      <v-card
+        class="mx-auto"
+        rounded
+      >
+        <v-card-text class="text--primary">
+          <div>
+            Présentez ce QR Code au service clientèle pour vous identifier.
+          </div>
+          <div class="text-center mt-2">
+            <qrcode :value="this.$auth.user.email" :options="{ width: 150 }" />
+          </div>
+          <br />
+          <v-btn
+            color="blue-grey"
+            class="white--text"
+            @click="logout"
+          >
+            Déconnexion
+            <v-icon
+              right
+            >
+              mdi-logout-variant
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  auth: true,
+  methods: {
+    async logout() {
+      await this.$auth.logout();
+      await this.$router.push('/');
+    }
+  }
+};
+</script>
---
+++ plugins/qr-code.ts
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+// @ts-ignore
+import VueQrCode from '@chenfengyuan/vue-qrcode';
+
+Vue.component(VueQrCode.name, VueQrCode);
